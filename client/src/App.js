7526dcb772028e019da44aaf0ec41c228851bd71
Cristian Hornedo <officialcristianl@gmail.com>
2020-11-04 13:13:27 -0500

    Added initial files and started the path of routing on app.js

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/* TODO make a navv component */}
+        <Switch>
+          <Route />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
