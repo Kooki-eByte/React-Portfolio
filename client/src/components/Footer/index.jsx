0764d01103543f790af7b6963791954d8b09fcda
Cristian Hornedo <officialcristianl@gmail.com>
2020-11-05 14:27:25 -0500

    Fixed the nav issue and got the footer and header components made

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <React.Fragment>
+            <footer className="footer">
+                <p>© 2020 - Site Built By Cristian Hornedo 🌱</p>
+            </footer>
+        </React.Fragment>
+    )
+}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,27 @@
 import React from "react"
 import { Col, Container, Row } from "../components/GridControl/Grid"
+// TODO -> import a styling to change the size of fonts and try to grab the image through css
 
 export default function Home() {
     return (
         <Container >
             <Row>
-                <Col size="md-6" >
-                    <h1>Hello, I am the Home Page</h1>
+                <Col size="lg-6 md-6 sm-12" >
+                    <img src="../assets/Images/MyPicture.jpeg" alt="Cristian" />
                 </Col>
-                <Col size="md-6">
-                    <h1>Hello, I am the Home Page side column</h1>
+                <Col size="lg-6 md-6 sm-12">
+                    <p>
+                        Hello, My name is Cristian Hornedo. I am a Full Stack Developer.
+                    </p>
+                    <p>
+                        Coding has been apart of my life since I was in highschool, Going back and forth with different ways of
+                        approaching a job when I stumbled upon web developement. I've been surfing the web a different way ever
+                        since!
+                    </p>
+                    <p>
+                        In Full Stack Developement I aim to appeal on all spectrums of what a person's dream is for what they
+                        want in a website and how to achieve such things.
+                    </p>
                 </Col>
             </Row>
         </Container>
